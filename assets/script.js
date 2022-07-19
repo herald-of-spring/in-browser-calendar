dac9b521d6e6bcef28fa20e7ba459f3ca12023e0
herald-of-spring <anhnguye@usc.edu>
2022-07-18 23:58:31 -0700

    time applied

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,37 @@
 var time = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
-var calendar = localStorage.getItem("today").split(",");
+var today = moment().format("MMMM Do, YYYY");
+var curHour = moment().format("HH");
+$("#currentDay").text(today);
+var calendar = localStorage.getItem(today);
 if (calendar === null) {
   calendar = ["", "", "", "", "", "", "", "", ""];    //9 time slots from 9 AM to 5 PM
 }
-for (var i=0; i<9; i++) {
-  createTimeBlock(i);
+else {
+  calendar = calendar.split(",");
 }
-
-function createTimeBlock(index) {
+for (var i=0; i<9; i++) {    //creating time blocks
   $("#all-times").append(
     $("<div>").addClass("time-block row").append(
-      $("<div>").addClass("hour col-md-1 align-middle").text(time[index])).append(
-      $("<textarea>").addClass("past col-md-10").val(calendar[index])).append(
-      $("<div>").addClass("saveBtn col-md-1").attr("index", index).append(
+      $("<div>").addClass("hour col-md-1 align-middle").text(time[i])).append(
+      $("<textarea>").addClass(function () {
+        var res = "col-md-10 ";
+        if (curHour < i + 9) {
+          res += "future";
+        }
+        else if (curHour == i + 9) {
+          res += "present";
+        }
+        else {
+          res += "past";
+        }
+        return res;
+      }).val(calendar[i])).append(
+      $("<div>").addClass("saveBtn col-md-1").attr("index", i).append(
         $("<i>").addClass("fas fa-save")
       )
       .on("click", function() {
-        calendar[index] = $(this).prev().val();
-        console.log(index, $(this).prev().val());
-        localStorage.setItem("today", calendar);
+        calendar[$(this).attr("index")] = $(this).prev().val();
+        localStorage.setItem(today, calendar);
         $("#confirmation").fadeIn(0).fadeOut(1500);
       })
     )
